435c4e492ad68a1863b86a31ca7a8712186f7825
Tara Craig <tara.craig7@gmail.com>
2019-03-03 19:52:33 -0700

    Add plants index route

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,5 @@
 class Plant < ApplicationRecord
-  validates_presence_of :name, :amount_of_light, :amount_of_water
+  validates_presence_of   :name,
+                          :amount_of_light,
+                          :amount_of_water
 end
